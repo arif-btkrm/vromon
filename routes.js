a1438f47cdafd4f5f6eb3826ad3569801673cb6c
arif-btkrm <arif.btkrm@gmail.com>
2023-09-13 05:33:55 +0600

    routerjs

--- routes.js
+++ routes.js
@@ -1,6 +1,22 @@
 const router = require('express').Router();
 const healthHandler = require('./routeHandler/healthHandler');
 
+// Health Check
 router.get('/health', healthHandler);
+
+router.get('/', homeHandler);
+
+router.get('/vehicle', vhicleHandler);
+
+router.get('/bus', busHandler);
+
+router.get('/health', healthHandler);
+
+router.get('/health', healthHandler);
+
+router.get('/health', healthHandler);
+
+router.get('/health', healthHandler);
+
 
 module.exports = router;
